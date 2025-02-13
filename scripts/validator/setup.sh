6c5331f90eb5509ff5fe5eb84604e33a97cdef2e
roberto mazo <mazziitoozz@gmail.com>
2025-02-13 21:57:46 +0100

    setup.sh

--- scripts/validator/setup.sh
+++ scripts/validator/setup.sh
@@ -104,10 +104,10 @@ success_msg "Python dependencies installed successfully."
 # ------------------------------------------------------------------
 # Step 10: Install autoppia_iwa Package
 echo -e "\e[34m[INFO]\e[0m Installing autoppia_iwa package..."
-if cd autoppia_iwa && uv pip install -e . && cd ..; then
-    success_msg "autoppia_iwa package installed successfully."
+if cd autoppia_iwa_module && uv pip install -e . && cd ..; then
+    success_msg "autoppia_iwa_module package installed successfully."
 else
-    handle_error "Failed to install autoppia_iwa package"
+    handle_error "Failed to install autoppia_iwa_module package"
 fi
 
 # ------------------------------------------------------------------
