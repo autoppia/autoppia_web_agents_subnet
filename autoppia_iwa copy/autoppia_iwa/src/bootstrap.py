f80f00e2bcb30b45b0539203196cdc1355f818e1
DaryxXx <mauricio.munozlopez@gmail.com>
2025-02-17 17:43:56 +0100

    iwa module

--- autoppia_iwa copy/autoppia_iwa/src/bootstrap.py
+++
@@ -1,9 +0,0 @@
-from .di_container import DIContainer
-
-
-class AppBootstrap:
-    def __init__(self):
-        self.configure_dependency_injection()
-
-    def configure_dependency_injection(self):
-        self.container = DIContainer()
--- autoppia_iwa copy/autoppia_iwa/src/web_agents/custom_apified_agent/agent.py
+++
@@ -1,6 +0,0 @@
-from ..apified_agent import ApifiedWebAgent
-
-
-class MyCustomApifiedWebAgent(ApifiedWebAgent):
-    def __init__(self, name: str, host: str, port: int):
-        super().__init__(name, host, port)
