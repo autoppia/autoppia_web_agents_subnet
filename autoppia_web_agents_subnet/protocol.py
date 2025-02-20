0b9d6c5423e8051490d64dd66b5da14c548f38ed
DaryxXx <mauricio.munozlopez@gmail.com>
2025-02-20 09:41:19 +0100

    stats

--- autoppia_web_agents_subnet/protocol.py
+++ autoppia_web_agents_subnet/protocol.py
@@ -5,6 +5,27 @@ from autoppia_iwa.src.execution.actions.actions import AllActionsUnion
 from autoppia_iwa.src.data_generation.domain.classes import Task
 
 
+class MinerStats(BaseModel):
+    avg_score: float = 0.0
+    avg_execution_time: float = 0.0
+    avg_evaluation_time: float = 0.0
+    total_tasks: int = 0
+    last_task: Optional["Task"] = None
+    sum_score: float = 0.0
+    sum_execution_time: float = 0.0
+    sum_evaluation_time: float = 0.0
+
+    def update(self, score: float, execution_time: float, evaluation_time: float, last_task: "Task"):
+        self.total_tasks += 1
+        self.sum_score += score
+        self.sum_execution_time += execution_time
+        self.sum_evaluation_time += evaluation_time
+        self.avg_score = self.sum_score / self.total_tasks
+        self.avg_execution_time = self.sum_execution_time / self.total_tasks
+        self.avg_evaluation_time = self.sum_evaluation_time / self.total_tasks
+        self.last_task = last_task
+
+
 class TaskSynapse(Synapse):
     version: str = ""
     prompt: str = ""
@@ -35,22 +56,3 @@ class FeedbackSynapse(Synapse):
         return self
 
 
-class MinerStats(BaseModel):
-    avg_score: float = 0.0
-    avg_execution_time: float = 0.0
-    avg_evaluation_time: float = 0.0
-    total_tasks: int = 0
-    last_task: Optional["Task"] = None
-    sum_score: float = 0.0
-    sum_execution_time: float = 0.0
-    sum_evaluation_time: float = 0.0
-
-    def update(self, score: float, execution_time: float, evaluation_time: float, last_task: "Task"):
-        self.total_tasks += 1
-        self.sum_score += score
-        self.sum_execution_time += execution_time
-        self.sum_evaluation_time += evaluation_time
-        self.avg_score = self.sum_score / self.total_tasks
-        self.avg_execution_time = self.sum_execution_time / self.total_tasks
-        self.avg_evaluation_time = self.sum_evaluation_time / self.total_tasks
-        self.last_task = last_task
