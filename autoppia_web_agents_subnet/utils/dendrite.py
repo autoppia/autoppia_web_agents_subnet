b94bbd236309f1e7e85abfcf27efec8dd09bbf41
DaryxXx <mauricio.munozlopez@gmail.com>
2025-02-20 12:38:45 +0100

    solve apified agent

--- autoppia_web_agents_subnet/utils/dendrite.py
+++ autoppia_web_agents_subnet/utils/dendrite.py
@@ -1,27 +1,23 @@
 import bittensor as bt
 from typing import List
-from autoppia_web_agents_subnet.protocol import TaskSynapse
-
+from autoppia_web_agents_subnet.protocol import TextSynapse
 
 async def dendrite_with_retries(
-    dendrite: bt.dendrite, 
-    axons: list, 
-    inputs: List[TaskSynapse],   # <-- CHANGED: now accepts a list
-    deserialize: bool, 
-    timeout: float, 
+    dendrite: bt.dendrite,
+    axons: list,
+    synapse: TextSynapse,
+    deserialize: bool,
+    timeout: float,
     cnt_attempts=3
-) -> List[TaskSynapse]:
-    res: List[TaskSynapse | None] = [None] * len(axons)
+) -> List[TextSynapse]:
+    res: List[TextSynapse | None] = [None] * len(axons)
     idx = list(range(len(axons)))
     axons = axons.copy()
 
     for attempt in range(cnt_attempts):
-        # Only send the subset of inputs corresponding to the current idx
-        attempt_inputs = [inputs[i] for i in idx]
-
-        responses: List[TaskSynapse] = await dendrite.forward(
-            endpoints=axons,
-            inputs=attempt_inputs,
+        responses: List[TextSynapse] = await dendrite(
+            axons=axons,
+            synapse=synapse,
             deserialize=deserialize,
             timeout=timeout
         )
@@ -32,7 +28,9 @@ async def dendrite_with_retries(
             if response.dendrite.status_code is not None and int(response.dendrite.status_code) == 422:
                 if attempt == cnt_attempts - 1:
                     res[idx[i]] = response
-                    bt.logging.info("Wasn't able to get answers from axon {} after {} attempts".format(axons[i], cnt_attempts))
+                    bt.logging.info(
+                        "Wasn't able to get answers from axon {} after {} attempts".format(axons[i], cnt_attempts)
+                    )
                 else:
                     new_idx.append(idx[i])
                     new_axons.append(axons[i])
@@ -40,12 +38,14 @@ async def dendrite_with_retries(
                 res[idx[i]] = response
 
         if len(new_idx):
-            bt.logging.info('Found {} responses with broken pipe, retrying them'.format(len(new_idx)))
+            bt.logging.info(
+                'Found {} synapses with broken pipe, retrying them'.format(len(new_idx))
+            )
         else:
             break
 
         idx = new_idx
         axons = new_axons
 
-    assert all([el is not None for el in res])
+    assert all(el is not None for el in res)
     return res
