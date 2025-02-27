6585e48262ecfa9260581057c7c5d60690857ecc
DaryxXx <mauricio.munozlopez@gmail.com>
2025-02-27 11:25:23 +0100

    mongo list

--- scripts/mongo/list.sh
+++ scripts/mongo/list.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-# Load environment variables from .env file
 export $(grep -v '^#' .env | xargs)
 
-# Check if MONGODB_URL is set
 if [ -z "$MONGODB_URL" ]; then
   echo "Error: MONGODB_URL is not set in the .env file"
   exit 1
 fi
 
-# Execute the MongoDB command using MONGODB_URL
 mongosh "$MONGODB_URL" --eval '
 db.getMongo().getDBNames().forEach(function(dbName) {
   if (["admin", "config", "local"].indexOf(dbName) === -1) {
