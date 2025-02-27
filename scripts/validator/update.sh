7d326a3e386377dcb02601de9b8bda7137706177
DaryxXx <mauricio.munozlopez@gmail.com>
2025-02-27 12:11:50 +0100

    visualizator

--- scripts/validator/update.sh
+++ scripts/validator/update.sh
@@ -1,5 +1,5 @@
 git pull 
-cd autoppia_iwa_module && git pull origin main && cd ..
+cd autoppia_iwa_module && git pull origin main && pip install -e autoppia_iwa_module && cd ..
 git submodule update --init --recursive
 scripts/mongo/deploy_docker_mongo.sh
-pip install -e autoppia_iwa_module
+pip install -e .
