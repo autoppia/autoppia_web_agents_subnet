ee2c6b8a06b92e55001f1a97903d3d07dbb8fdff
DaryxXx <mauricio.munozlopez@gmail.com>
2025-03-11 15:35:59 +0100

    refactor

--- autoppia_web_agents_subnet/validator/utils.py
+++ autoppia_web_agents_subnet/validator/utils.py
@@ -3,9 +3,7 @@ from autoppia_iwa.src.demo_webs.classes import WebProject
 from autoppia_iwa.src.demo_webs.utils import initialize_demo_webs_projects
 from autoppia_iwa.src.demo_webs.config import demo_web_projects
 
-from autoppia_web_agents_subnet.protocol import (
-    MinerStats,
-)
+from autoppia_web_agents_subnet.protocol import MinerStats, TaskSynapse
 from autoppia_web_agents_subnet.utils.logging import ColoredLogger
 from autoppia_web_agents_subnet.validator.config import (
     TIME_WEIGHT,
@@ -234,3 +232,52 @@ async def retrieve_random_demo_web_project() -> WebProject:
         ColoredLogger.YELLOW,
     )
     return project
+
+
+async def dendrite_with_retries(
+    dendrite: bt.dendrite,
+    axons: list,
+    synapse: TaskSynapse,
+    deserialize: bool,
+    timeout: float,
+    cnt_attempts=3
+) -> List[TaskSynapse]:
+    res: List[TaskSynapse | None] = [None] * len(axons)
+    idx = list(range(len(axons)))
+    axons = axons.copy()
+
+    for attempt in range(cnt_attempts):
+        responses: List[TaskSynapse] = await dendrite(
+            axons=axons,
+            synapse=synapse,
+            deserialize=deserialize,
+            timeout=timeout
+        )
+
+        new_idx = []
+        new_axons = []
+        for i, response in enumerate(responses):
+            if response.dendrite.status_code is not None and int(response.dendrite.status_code) == 422:
+                if attempt == cnt_attempts - 1:
+                    res[idx[i]] = response
+                    bt.logging.info(
+                        "Wasn't able to get answers from axon {} after {} attempts".format(axons[i], cnt_attempts)
+                    )
+                else:
+                    new_idx.append(idx[i])
+                    new_axons.append(axons[i])
+            else:
+                res[idx[i]] = response
+
+        if len(new_idx):
+            bt.logging.info(
+                'Found {} synapses with broken pipe, retrying them'.format(len(new_idx))
+            )
+        else:
+            break
+
+        idx = new_idx
+        axons = new_axons
+
+    assert all(el is not None for el in res)
+    return res
