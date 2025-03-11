ee2f767b1e0c1769074332b184cb49443025f021
dr riiveer <59394472+Riiveer@users.noreply.github.com>
2025-03-11 11:57:11 +0100

    created a script ot make pull

---
+++ scripts/dev/pull.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# Actualizar el submódulo webs_demo
+SUBMODULE_PATH="autoppia_iwa_module/modules/webs_demo"
+cd "$SUBMODULE_PATH" || { echo "El directorio $SUBMODULE_PATH no existe"; exit 1; }
+git pull
+
+# Volver al directorio del submódulo principal y actualizarlo
+cd ../..
+git pull
+
+# Volver al directorio raíz del proyecto
+cd ..
+
+git pull
+
+echo "Actualización completada para webs_demo, autoppia_iwa_module y el repositorio raíz."
