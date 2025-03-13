448a7193ec2c8833b48312260f46c874a753eb39
dr riiveer <59394472+Riiveer@users.noreply.github.com>
2025-03-13 17:14:37 +0100

    updates

--- autoppia_web_agents_subnet/validator/forward.py
+++ autoppia_web_agents_subnet/validator/forward.py
@@ -290,7 +290,9 @@ async def process_tasks(
         bt.logging.debug(f"Task tests: {task.tests}")
 
         # 1) Choose a random subset of miners.In this case the whole subnet.
-        miner_uids = np.array([101, 102, 103])  # get_random_uids(validator, k=SAMPLE_SIZE)
+        miner_uids = np.array(
+            [101, 102, 103]
+        )  # get_random_uids(validator, k=SAMPLE_SIZE)
         bt.logging.info(f"Miner UIDs chosen: {miner_uids}")
         miner_axons = [validator.metagraph.axons[uid] for uid in miner_uids]
 
@@ -351,6 +353,14 @@ async def process_tasks(
         ColoredLogger.info(
             f"test result matrices: --> {test_results_matrices} ",
             ColoredLogger.RED,
+        )
+        ColoredLogger.info(
+            f"REWARDS: --> {rewards} ",
+            ColoredLogger.YELLOW,
+        )
+        ColoredLogger.info(
+            f"REWARDS: --> {miner_uids} ",
+            ColoredLogger.BLUE,
         )
         end_eval = time.time()
         bt.logging.info(f"Miners final rewards: {rewards}")
