e94bfbc4e2328ea26b711be2117e15f804c15347
DaryxXx <mauricio.munozlopez@gmail.com>
2025-03-13 12:27:56 +0100

    bug

--- neurons/miner.py
+++ neurons/miner.py
@@ -78,12 +78,12 @@ class Miner(BaseMinerNeuron):
 
     async def forward(self, synapse: TaskSynapse) -> TaskSynapse:
 
-        # Checking Weights Versio
-        # version_check = is_version_in_range(synapse.version, self.version, self.least_acceptable_version)
+        # Checking Weights Version
+        version_check = is_version_in_range(synapse.version, self.version, self.least_acceptable_version)
 
-        # if not version_check:
-        #     ColoredLogger.info(f"Not reponding due to version check: {synapse.version} not between {self.least_acceptable_version} - { self.version}. This is intended behaviour", "yellow")
-        #     return synapse
+        if not version_check:
+            ColoredLogger.info(f"Not reponding due to version check: {synapse.version} not between {self.least_acceptable_version} - { self.version}. This is intended behaviour", "yellow")
+            return synapse
 
         try:
             start_time = time.time()
