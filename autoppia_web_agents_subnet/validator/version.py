703ad73608ba639d624f9b7c7a24c5f8d24cde26
DaryxXx <mauricio.munozlopez@gmail.com>
2025-03-24 12:20:58 +0100

    fixed expection raising

--- autoppia_web_agents_subnet/validator/version.py
+++ autoppia_web_agents_subnet/validator/version.py
@@ -1,3 +1,4 @@
+import bittensor as bt
 import random
 from typing import List
 from autoppia_web_agents_subnet.utils.weights_version import generate_random_version
@@ -7,21 +8,24 @@ from autoppia_web_agents_subnet.utils.logging import ColoredLogger
 
 
 async def check_miner_not_responding_to_invalid_version(self, task_synapse:TaskSynapse, miner_axons, probability:float, timeout:int):
-    do_check_versions = random.random() < probability
+    try:
+        do_check_versions = random.random() < probability
 
-    version_responses = []
-    if do_check_versions:
-        random_version = generate_random_version(self.version, self.least_acceptable_version)
-        task_synapse.version = random_version
-        ColoredLogger.info(f"Sending check version synapses with random version {random_version}", "yellow")
-        responses: List[TaskSynapse] = await dendrite_with_retries(
-            dendrite=self.dendrite,
-            axons=miner_axons,
-            synapse=task_synapse,
-            deserialize=True,
-            timeout=timeout,
-        )
-        version_responses.extend(responses)
-    else:
-        version_responses.extend([TaskSynapse(prompt="", url="", actions=[]) for _ in range(len(miner_axons))])
-    return version_responses
+        version_responses = []
+        if do_check_versions:
+            random_version = generate_random_version(self.version, self.least_acceptable_version)
+            task_synapse.version = random_version
+            ColoredLogger.info(f"Sending check version synapses with random version {random_version}", "yellow")
+            responses: List[TaskSynapse] = await dendrite_with_retries(
+                dendrite=self.dendrite,
+                axons=miner_axons,
+                synapse=task_synapse,
+                deserialize=True,
+                timeout=timeout,
+            )
+            version_responses.extend(responses)
+        else:
+            version_responses.extend([TaskSynapse(prompt="", url="", actions=[]) for _ in range(len(miner_axons))])
+        return version_responses
+    except Exception as e:
+        bt.logging.error(f"Error while sending version synapses: {e}")
