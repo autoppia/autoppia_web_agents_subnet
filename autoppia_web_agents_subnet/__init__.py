edfcd141f965718624029a517d254d790b9542af
Riiveer <mazziitoozz@gmail.com>
2026-02-19 17:06:27 +0100

    update web version

--- autoppia_web_agents_subnet/__init__.py
+++ autoppia_web_agents_subnet/__init__.py
@@ -1,5 +1,5 @@
 SUBNET_IWA_VERSION = "12.1.0"
-WEBS_DEMO_VERSION = "12.0.0"
+WEBS_DEMO_VERSION = "12.1.0"
 __least_acceptable_version__ = "11.0.0"
 version_split = SUBNET_IWA_VERSION.split(".")
 version_url = "https://raw.githubusercontent.com/autoppia/autoppia_web_agents_subnet/main/autoppia_web_agents_subnet/__init__.py"
